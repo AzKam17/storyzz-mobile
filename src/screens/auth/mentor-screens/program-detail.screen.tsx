c67fc25966c372657da1f7053f2c89fcb749fe87
AmadouAzizKAMAGATE <aziz.kamagate@djamo.io>
2025-08-24 15:00:36 +0000

    fix(program-detail): use program cover image instead of default image

--- src/screens/auth/mentor-screens/program-detail.screen.tsx
+++ src/screens/auth/mentor-screens/program-detail.screen.tsx
@@ -136,7 +136,7 @@ export const ProgramDetailScreen = React.memo(function (props: Props) {
         <>
           <YStack gap={20} paddingVertical={15}>
             <Image
-              source={require("~/images/default-image-women-large.png")}
+              source={program.cover}
               style={{ width: "100%", height: 215, borderRadius: 10 }}
             />
             <ProgramTitleText>{program.programName}</ProgramTitleText>
